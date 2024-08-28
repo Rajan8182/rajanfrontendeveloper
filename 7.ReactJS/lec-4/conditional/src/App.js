3a2f85cefa7db8847c2d26599c51e51909392a92
Rajan8182 <rajanchovatiya27@gmail.com>
2024-08-28 17:49:09 +0530

    lecture-4

---
+++ 7.ReactJS/lec-4/conditional/src/App.js
@@ -0,0 +1,13 @@
+import React from "react"
+import "./App.css" 
+import Conditional from "./Conditional.jsx"
+
+const App = () => {
+  return (
+    <div>
+       <Conditional/>
+    </div>
+  )
+}
+
+export default App
